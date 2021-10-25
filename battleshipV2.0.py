01a4e919d5d91303951d302553364a63c8050824
PetoB000 <88715505+PetoB000@users.noreply.github.com>
2021-10-25 13:47:38 +0200

    get_shoot function added

--- battleshipV2.0.py
+++ battleshipV2.0.py
@@ -17,6 +17,13 @@ def display_board(board):
         print('')
 
 
+def get_shoot():
+    move = input("Give a coordinate:")
+    valid = validate_coordinates()
+    if valid == True:
+        row, col = move[0], move[1]
+        return row, col
+
 def battleship_main():
     # start menu()       5*5-ös pálya ( 2*2 flotta, meg 2*1 flotta ), plusz üdvözlés, meg egy kilépési lehetőség    >  Marcsi
     # board = init_board(size=5)      pálya létrehozása              
