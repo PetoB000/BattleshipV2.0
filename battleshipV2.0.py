5369d7caa1b5894a4b984ae01222caa9674ece97
Vitályos Márton <marci12345678956@gmail.com>
2021-10-25 13:10:25 +0200

    planning the game

--- battleshipV2.0.py
+++ battleshipV2.0.py
@@ -1,3 +1,31 @@
 def init_board(size):
     board = [["0" for _ in range(size)] for _ in range(size)]
     return board
+
+
+
+
+
+
+def battleship_main():
+    # start menu()       5*5-ös pálya ( 2*2 flotta, meg 2*1 flotta ), plusz üdvözlés, meg egy kilépési lehetőség    >  Marcsi
+    # board = init_board(size=5)      pálya létrehozása              
+    # display_init_board(board)       pálya megjelenítése    > Marci
+    # placement_phase(board1, board2)
+        # ask_fleets()                                      > Zsu
+        # validate_coordinates()
+
+    # get_shoot()
+        # input kérés 
+        # validate coordinates()                           > Balázs
+        # return row col 
+
+    # hit_confirm(board1, board2, row, col)               > Balázs 
+    # game_logic()                                          > az egész csapat
+        pass
+
+
+
+
+if __name__ == "__main__":
+    battleship_main()
