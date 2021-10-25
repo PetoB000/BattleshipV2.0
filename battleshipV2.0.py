a2574d447a7700cf004afc54c51d9133173295fa
ahpmh <alwayshappypeople@gmail.com>
2021-10-25 13:55:26 +0200

    start menu

--- battleshipV2.0.py
+++ battleshipV2.0.py
@@ -1,9 +1,65 @@
+import time, os
+
 def init_board(size):
     board = [["0" for _ in range(size)] for _ in range(size)]
     return board
 
 
 
+def start_menu():
+    print(
+"""
+
+
+
+        Hi, and welcome to the Battleship game!
+
+  
+
+                                        The board size is 5 x 5.
+
+                                        You have to place 2 small ships(2x1) and 2 big ships (2x2).
+
+                                        The faster who find the other player's ships is the winner!
+
+
+        Please choose from the following options:
+
+                \ \_____
+                ###[==_____>  1. Play Battleship Human vs. Human
+                /_/      __
+                            \ \_____
+                        ###[==_____  > 2. Quit
+                            /_/
+
+
+
+""")
+
+# ha kivalasztotta a jatekomodot sleep 1sec, feljon a 2. ASCII mondjuk sleep 3 sec aztan indul a jatek 
+
+"""
+Let's play!
+     ....
+         ,''. :   __
+             \|_.'  `:       _.----._//_
+            .'  .'.`'-._   .'  _/ -._ \)-.----O
+           '._.'.'      '--''-'._   '--..--'-`
+            .'.'___    /`'---'. / ,-'`
+ssss      _<__.-._))../ /'----'/.'_____:'.
+   \_    :            \ ]              :  '.
+     \___:             \\              :    '.
+         :              \\__           :    .'
+         :_______________|__]__________:  .'
+                    .' __ '.           :.'
+                  .' .'  '. '.
+                .' .'      '. '.
+              .' .'          '. '.
+           _.' .'______________'. '._
+          [_0______________________0_]
+          
+          
+          """
 
 
 
